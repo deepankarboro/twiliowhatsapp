11a20dd3db70a610c7c3f7728e8a69c0fe9c4623
Deepankar Boro <59099012+deepankarboro@users.noreply.github.com>
2020-06-01 01:19:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ def hello():
 
 @app.route('/bot', methods=['POST'])
 def bot():
-    incoming_msg = request.values.get('Body', '').lower()
+    incoming_msg = request.values.get('Body', '').lower() #Gets the incoming message from user and stores it.
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
@@ -23,22 +23,21 @@ def bot():
             quote = f'{data["content"]} ({data["author"]})'
         else:
             quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
+        msg.body(quote) #Putting the quote as reply in message body
         responded = True
     if 'dog' in incoming_msg or 'puppy' in incoming_msg or 'doggie' in incoming_msg:
         # return a dog pic
         contents = requests.get('https://random.dog/woof.json').json()
         url = contents['url']
-        msg.media(url)
+        msg.media(url) #Putting dog image as reply in message body
         responded = True
     if 'cat' in incoming_msg:
         # return a cat pic
-        b=msg.media('https://cataas.com/cat')
+        b=msg.media('https://cataas.com/cat') #Putting cat image as reply in message body
         responded = True
     if not responded:
         msg.body('I only know about famous quotes, dogs and cats, sorry!')
-    return str(resp)
-
+    return str(resp) #Returning message response to user
 
 if __name__ == '__main__':
     app.run()
